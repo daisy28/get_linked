4d3cf18e0870a41f44b90f50fdb96ebd8fc2a354
Gloria <ubahgloria16@gmail.com>
2023-09-20 00:46:16 +0100

    Setup complete

---
+++ getLinked/src/App.jsx
@@ -0,0 +1,15 @@
+// import React from 'react'
+
+
+function App() {
+
+  return (
+    <section className={`underline text-9xl text-white`}>
+      hello world!
+      <p className={`text-amber-900 font-extrabold`}>work na</p>
+      <p className={`text-amber-900 font-extrabold`}>work na</p>
+    </section>
+  )
+}
+
+export default App;
