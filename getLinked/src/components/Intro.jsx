3b8d37827881b45c2f6c6a5acc434094ff810e1b
Gloria <ubahgloria16@gmail.com>
2023-09-20 23:39:15 +0100

    Feat: Completed the Intro section

--- getLinked/src/components/Intro.jsx
+++ getLinked/src/components/Intro.jsx
@@ -1,7 +1,51 @@
-
+import Idea from "../assets/The big idea.svg";
+import Arrow from "../assets/arrow.svg";
+import Star from "../assets/star pu.svg";
 
 const Intro = () => {
-     return(
-          <section></section>
-     )
-}
+  return (
+    <section
+      className={`p-6 bg-[#140D27] border-b border-[rgba(129,128,128,0.24)] lg:px-[3rem]`}
+    >
+      <div className={`md:flex justify-center gap-6 items-center xl:gap-0`}>
+        <div className={`mb-[4rem] md:mb-0 relative md:w-[50%]`}>
+          <img src={Idea} alt="ideas" />
+          <img
+            src={Arrow}
+            alt="curly arrow"
+            className={`absolute w-[30px] left-[45%] bottom-[-30px] md:bottom-[10px] md:left-[310px] lg:left-[390px] xl:left-[430px]`}
+          />
+          <img
+            src={Star}
+            alt="star"
+            className={`absolute left-[10px] top-[108px] w-[12px] animate-ping delay-300 duration-100 ease-in md:top-[140px] md:left-0`}
+          />
+        </div>
+        <div className={`my-4 text-center md:text-left relative md:w-[50%]`}>
+          <img
+            src={Star}
+            alt="star"
+            className={`absolute right-0 top-[40px] w-[8px] animate-ping delay-300 duration-100 ease-in md:top-[20px] md:right-[60px]`}
+          />
+          <h1
+            className={`text-[24px] font-[600] lading-[32px] text-[#fff] mb-4`}
+          >
+            Introduction to getlinked{" "}
+            <span className={`text-[#D434FE]`}>techHackathon 1.0</span>
+          </h1>
+          <p className={`text-[13px] font-[400] text-[#fff] leading-[30px]`}>
+            Our tech hackathon is a melting point pot of visionaries, and its
+            purpose is as clear as day; to shape the future. Whether you&apos;re
+            a coding genuis, a design maverick, or a concept wizard, you&apos;ll
+            have the chance to transform your ideas into reality. Solving
+            real-world problems, pushing the boundaries of technology, and
+            creating solution that can change the world, that&apos;s what
+            we&apos;re all about
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Intro;
--- getLinked/src/pages/LandingPage.jsx
+++ getLinked/src/pages/LandingPage.jsx
@@ -1,12 +1,14 @@
 // import React from "react";
 import LandingPageNav from "../components/LandingPageNav";
 import Hero from "../components/Hero";
+import Intro from "../components/Intro"
 
 const LandingPage = () => {
      return (
           <section className={``}>
                <LandingPageNav />
                <Hero />
+               <Intro />
           </section>
      )
 }
