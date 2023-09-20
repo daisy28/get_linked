6e300dd7c950909f4a8604c7220b1931300a79e8
Gloria <ubahgloria16@gmail.com>
2023-09-20 02:57:32 +0100

    Project setup

--- getLinked/src/App.jsx
+++ getLinked/src/App.jsx
@@ -1,15 +1,33 @@
-// import React from 'react'
+import React from 'react'
+import { createHashRouter, RouterProvider, Outlet } from "react-router-dom"
+import LandingPage from './pages/LandingPage';
+import Contact from './pages/Contact';
+import Register from './pages/Register';
+
 
 
 function App() {
 
+  const router = createHashRouter([
+    {
+      path: "/",
+      element: <LandingPage />
+    },
+    {
+      path: "/contact",
+      element: <Contact />
+    },
+    {
+      path: "/register",
+      element: <Register />
+    }
+  ]);
+
   return (
-    <section className={`underline text-9xl text-white`}>
-      hello world!
-      <p className={`text-amber-900 font-extrabold`}>work na</p>
-      <p className={`text-amber-900 font-extrabold`}>work na</p>
-    </section>
-  )
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
---
+++ getLinked/src/pages/LandingPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LandingPage = () => {
+     return (
+          <section className={`font-face-cd bg-[#140D27] text-[#D434FE]`}>
+               LandingPage
+          </section>
+     )
+}
+
+export default LandingPage;
